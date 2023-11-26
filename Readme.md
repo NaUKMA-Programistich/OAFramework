65b8ed8212d4d39c916b3599b6afd80f3cb75dc6
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-11-26 20:45:33 +0200

    Google Login Docs

--- Readme.md
+++ Readme.md
@@ -2,27 +2,59 @@
 
 ## Google
 
-Setup
-----
+### Setup
 
 * Get OAuth Client ID and URL https://console.cloud.google.com/
 
+Example:
+
+Client ID: **1234567890-abcdefghijklmnopqrstuvwxyz.apps.googleusercontent.com**
+
+URL: **com.googleusercontent.apps.1234567890-abcdefghijklmnopqrstuvwxyz**
+
 * Add Google Client ID to Info.plist
-<key>GIDClientID</key>
-<string>YOUR_IOS_CLIENT_ID</string>
+```xml
+    <key>GIDClientID</key>
+    <string>YOUR_IOS_CLIENT_ID</string>
+```
 
 * Add back URL to Schemes
+```xml
+<key>CFBundleURLTypes</key>
+<array>
+  <dict>
+    <key>CFBundleURLSchemes</key>
+    <array>
+      <string>YOUR_DOT_REVERSED_IOS_CLIENT_ID</string>
+    </array>
+  </dict>
+</array>
 
-Code
-----
+```
 
 ### SwiftUI
 
-* Call GoogleOAuth.shared.handleLink(url: url) in openUrl
-* Call GoogleOAuth.shared.startProcessSignIn on action
+* Call `GoogleOAuth.shared.handleLink(url: url)` in `openUrl`
+* Call `GoogleOAuth.shared.startProcessSignIn` on action
+
+```swift
+@main
+struct OAuthExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Button("Google Login") {
+                GoogleOAuth.shared.startProcessSignIn { user, error in }
+            }
+            .onOpenURL(perform: { url in
+                GoogleOAuth.shared.handleLink(url: url)
+            })
+        }
+    }
+}
+```
 
 ### UIKIT
 
-* Call GoogleOAuth.shared.handleLink(url: url) in AppDelegate by method application:openURL:options
-* Call GoogleOAuth.shared.startProcessSignIn on action
+* Call `GoogleOAuth.shared.handleLink(url: url)` in AppDelegate by method `application:openURL:options`
+* Call `GoogleOAuth.shared.startProcessSignIn` on action
 
--- Sources/OAFramework/GoogleOAuth.swift
+++ Sources/OAFramework/GoogleOAuth.swift
@@ -3,67 +3,79 @@ import GoogleSignIn
 import Logging
 
 /**
- Simple typealias for process result
+ Typealias represent callback for Google OAuth
+ - Parameters:
+    - user: Google user object if the authentication is successful, otherwise `nil`.
+    - error: An `Error` object if there is an error during the authentication process, otherwise `nil`.
  */
 public typealias GoogleOAuthCallback = (GIDGoogleUser?, Error?) -> Void
 
 /**
-    Base class for auth in Google Account
+ Base class for Google OAuth
  */
 public class GoogleOAuth: SignIn {
+
     private let logger = Logger(label: "GoogleOAuth")
 
+    /// Shared instance of GoogleOAuth
     public static let shared = GoogleOAuth()
 
-    /**
-     Google client ID setup from Info.plist and get from https://console.cloud.google.com/
-     */
+    /// Google client ID obtained from Info.plist. Obtain from https://console.cloud.google.com/
     private let clientID: String
 
     /**
-     Constructor with check exist client id
+     Initializes the GoogleOAuth instance.
      */
     private init() {
-        // Check client ID exist
-        if let path = Bundle.main.path(forResource: "Info", ofType: "plist"), let infoDict = NSDictionary(contentsOfFile: path), let gidClientID = infoDict["GIDClientID"] as? String {
+        // Check if client ID exists in Info.plist
+        if let path = Bundle.main.path(forResource: "Info", ofType: "plist"),
+           let infoDict = NSDictionary(contentsOfFile: path),
+           let gidClientID = infoDict["GIDClientID"] as? String {
             logger.info("Google client ID: \(gidClientID)")
-            self.clientID =  gidClientID
+            self.clientID = gidClientID
         } else {
-            logger.info("Google client ID not exist")
+            logger.info("Google client ID does not exist")
             self.clientID = ""
         }
-        // Check URL deeplink setup hiden in GoogleSignIn library
+        // Check URL deeplink setup (hidden in GoogleSignIn library)
     }
 
     /**
-     Entry point for login
+     Entry point for initiating the sign-in process.
+     - Parameters:
+        - callback: The callback with GoogleOAuthCallback
      */
     public func startProcessSignIn(callback: @escaping GoogleOAuthCallback) {
         logger.info("#startProcessSignIn")
+
+        // If there is a previous sign-in, attempt to restore it
         if GIDSignIn.sharedInstance.hasPreviousSignIn() {
             logger.info("hasPreviousSignIn")
             GIDSignIn.sharedInstance.restorePreviousSignIn(callback: callback)
             return
         }
-        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
-        guard let viewController = windowScene.windows.first?.rootViewController else { return }
-        let configuration = GIDConfiguration(clientID: clientID)
 
+        // Get the window scene and root view controller
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let viewController = windowScene.windows.first?.rootViewController else { return }
+
+        // Configure GIDSignIn and initiate the sign-in process
+        let configuration = GIDConfiguration(clientID: clientID)
         logger.info("signIn")
         GIDSignIn.sharedInstance.signIn(with: configuration, presenting: viewController, callback: callback)
     }
 
     /**
-     Handle deeplink
+     Handles deep links related to the sign-in process.
+     - Parameter url: The URL received by the app to handle the deep link.
      */
     public func handleLink(url: URL) {
         logger.info("#handleLink")
         GIDSignIn.sharedInstance.handle(url)
     }
 
-
     /**
-     Log out from local Google
+     Logs out from the local Google account.
      */
     public func signOut() {
         logger.info("#signOut")
@@ -71,6 +83,7 @@ public class GoogleOAuth: SignIn {
     }
 }
 
+/// Protocol indicating conformance to sign-in functionality.
 protocol SignIn {
 
 }
